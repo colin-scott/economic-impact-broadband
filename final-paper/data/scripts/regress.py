927cf3acd555dc60488234810034f1711ab48609
colin-scott <cs@cs.berkeley.edu>
2012-11-24 16:53:19 -0800

    Adding differentiated analysis

--- final-paper/data/scripts/regress.py
+++ final-paper/data/scripts/regress.py
@@ -24,6 +24,31 @@ LAC = 0
 # TODO(cs): dunno what this is
 mu = 0
 
-GDP_8006 = a_0 + a_1 * GDP_80 + a_2 * (I_Y_8006) + \
-           a_3 * TELEPEN_8006 + a_4 * PRIM_80 + \
-           a_5 * SSA + a_6 * LAC + mu
+def basic_regression():
+  GDP_8006 = a_0 + a_1 * GDP_80 + a_2 * (I_Y_8006) + \
+             a_3 * TELEPEN_8006 + a_4 * PRIM_80 + \
+             a_5 * SSA + a_6 * LAC + mu
+
+def differentiated_regresssion():
+ # We also divided the sample into developed and develop-
+ # ing economies (the latter including both middle-income and low-income
+ # countries according to the World Bank country classifications),
+ # created dummy variables, and generated the new variables TELEPENH and
+ # TELEPENL (the product of the dummy variables and the telecommunications
+ # penetration variables)
+
+def tele_regression():
+  # Number of main lines
+  FIXED = 0
+  # Number of mobile suscribers
+  MOBILE = 0
+  # Number of Internet users
+  INTERNET = 0
+  # Number of Broadband susccribers
+  BBND = 0
+  # High-income countries
+  H = 0
+  # Low and middle income countries
+  L = 0
+
+# TODO(cs): what is a t-statistic?
